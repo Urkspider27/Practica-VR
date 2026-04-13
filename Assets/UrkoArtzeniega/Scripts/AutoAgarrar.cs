1958ba6f369374c0396418801056fa2a17f73578
Urko Artzeniega <urko_artzeniega@euneiz.com>
2026-04-13 22:01:05 +0200

    Ultimo intento del dia
    Las espadas no se quedan en la mano ya, llevo 2 dias con ese bug no puedo maaaaas

---
+++ Assets/UrkoArtzeniega/Scripts/AutoAgarrar.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class AutoAgarrar : MonoBehaviour
+{
+    // Ahora usamos GameObject para que te deje arrastrar CUALQUIER COSA de la jerarquía
+    public GameObject mando;
+    public GameObject espada;
+
+    void Start()
+    {
+        var interactor = mando.GetComponent<XRDirectInteractor>();
+        var interactable = espada.GetComponent<XRGrabInteractable>();
+
+        if (interactor != null && interactable != null)
+        {
+            // Forzamos el agarre manual
+            interactor.StartManualInteraction((IXRSelectInteractable)interactable);
+        }
+        else
+        {
+            Debug.LogError("Oye, te falta el componente XRDirectInteractor en el mando o el XRGrabInteractable en la espada!");
+        }
+    }
+}
