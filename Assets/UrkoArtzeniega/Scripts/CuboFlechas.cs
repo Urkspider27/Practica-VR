b14206523fd7285b458f425517e232b40757d30b
Urko Artzeniega <urko_artzeniega@euneiz.com>
2026-05-05 16:31:07 +0200

    Rotacion clara
    Ahora he hecho que se note hacia direccion hay que cortar los cubosflecha

--- Assets/UrkoArtzeniega/Scripts/CuboFlechas.cs
+++ Assets/UrkoArtzeniega/Scripts/CuboFlechas.cs
@@ -7,25 +7,26 @@ public class CuboFlechas : MonoBehaviour
     public Direccion direccionRequerida;
 
     // El spawner asignará esto al crear el cubo
-    public GameObject flechaVisual; // arrastra el hijo en el Inspector
+    public GameObject flechaVisual;
 
     public void SetDireccion(Direccion dir)
     {
         direccionRequerida = dir;
 
+        // Usamos localRotation para que rote sobre el propio cubo y no se descoloque
         switch (dir)
         {
             case Direccion.Arriba:
-                flechaVisual.transform.rotation = Quaternion.Euler(0, 0, 0);
+                flechaVisual.transform.localRotation = Quaternion.Euler(0, 0, 0);
                 break;
             case Direccion.Abajo:
-                flechaVisual.transform.rotation = Quaternion.Euler(0, 0, 180);
+                flechaVisual.transform.localRotation = Quaternion.Euler(0, 0, 180);
                 break;
             case Direccion.Izquierda:
-                flechaVisual.transform.rotation = Quaternion.Euler(0, 0, 90);
+                flechaVisual.transform.localRotation = Quaternion.Euler(0, 0, 90);
                 break;
             case Direccion.Derecha:
-                flechaVisual.transform.rotation = Quaternion.Euler(0, 0, -90);
+                flechaVisual.transform.localRotation = Quaternion.Euler(0, 0, -90);
                 break;
         }
     }
